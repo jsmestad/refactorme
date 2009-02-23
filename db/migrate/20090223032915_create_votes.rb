e2a7e8bf1139cb0784eba4de9ec44c6eaa919d97
Brian Racer <brian@jetpackweb.com>
2009-02-22 21:48:06 -0600

    Vote model and migration

---
+++ db/migrate/20090223032915_create_votes.rb
@@ -0,0 +1,15 @@
+class CreateVotes < ActiveRecord::Migration
+  def self.up
+    create_table :votes do |t|
+      t.belongs_to :user
+      t.belongs_to :snippet
+      t.integer :score
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :votes
+  end
+end
