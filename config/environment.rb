b9a5a053ded1fcda4d9bf9a2e57d167503264b0d
jsmestad <justin.smestad@gmail.com>
2009-03-13 16:57:51 -0600

    fixed env

--- config/environment.rb
+++ config/environment.rb
@@ -17,15 +17,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "haml"
   config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
   config.gem "authlogic"
-  #config.gem "nokogiri"
-  #config.gem "bcrypt-ruby", :lib => 'bcrypt'
+  config.gem "nokogiri"
+  config.gem "bcrypt-ruby", :lib => 'bcrypt'
   config.gem "simplificator-tls-support", :lib => 'tls-support', :source => "http://gems.github.com"
   config.gem "mislav-will_paginate", :lib => 'will_paginate', :source => "http://gems.github.com"
   config.gem "chriseppstein-compass", :lib => 'compass', :source => 'http://gems.github.com'
