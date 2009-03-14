0e04ab6ed8627d6f40a79756478a91d7ddb6121c
Jacob Fugal <lukfugl@gmail.com>
2009-03-13 23:37:36 -0600

    update some expectations about redirect targets

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,7 +13,7 @@ describe UsersController do
       post :create, :user => { :login => "ben", :password => "benrocks", :password_confirmation => "benrocks", :email => "myemail@email.com" }
     }.should change(User, :count)
     
-    response.should redirect_to(account_path)
+    response.should redirect_to(root_path)
   end
   
   describe "given a user exists" do
