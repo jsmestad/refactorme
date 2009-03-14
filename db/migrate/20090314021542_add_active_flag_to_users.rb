d289b48ca19503603aeba50a62e3e5aed4b25db6
jsmestad <justin.smestad@gmail.com>
2009-03-13 20:34:57 -0600

    activations working

---
+++ db/migrate/20090314021542_add_active_flag_to_users.rb
@@ -0,0 +1,9 @@
+class AddActiveFlagToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :active, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :users, :active
+  end
+end
