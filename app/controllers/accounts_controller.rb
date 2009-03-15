471eef4688bf38ce310a6d45a8f156858c0abd3b
bobbyw <bobby@entryway.net>
2009-03-15 00:38:37 -0600

    separated methods into user and account controllers

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,18 @@
+class AccountsController < ApplicationController
+  before_filter :require_user, :only => [:edit, :update]
+
+  def edit
+    @user = @current_user
+  end
+
+  def update
+    @user = @current_user # makes our views "cleaner" and more consistent
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Account updated!"
+      redirect_to account_url
+    else
+      render :action => :edit
+    end
+  end
+  
+end
