00f19cfb17129e828cdb8b473ab3e237e35ac836
jsmestad <justin.smestad@gmail.com>
2009-03-23 23:02:46 -0600

    upgraded cucumber as well, ugh.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,9 @@
 # Sets up the Rails environment for Cucumber
-ENV["RAILS_ENV"] = "test" 
+ENV["RAILS_ENV"] ||= "test"
 require File.expand_path(File.dirname(__FILE__) + '/../../config/environment')
 require 'cucumber/rails/world'
-require 'cucumber/formatters/unicode'
+require 'cucumber/formatters/unicode' # Comment out this line if you don't want Cucumber Unicode support
+Cucumber::Rails.use_transactional_fixtures
 
 require 'webrat'
 require 'factory_girl'
@@ -14,6 +15,5 @@ Webrat.configure do |config|
   config.mode = :rails
 end
 
-# Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
-require 'webrat/rspec-rails'
+require 'webrat/core/matchers'
