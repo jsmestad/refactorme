2fe9aeb26765be36738866601bb279cd64b0e396
jsmestad <justin.smestad@gmail.com>
2009-03-23 22:48:19 -0600

    updated dependencies for authlogic changes, maybe it would be nice if he documented a thousand line commit

--- vendor/gems/nokogiri-1.2.3/ext/nokogiri/html_entity_lookup.h
+++
@@ -1,8 +0,0 @@
-#ifndef NOKOGIRI_HTML_ENTITY_LOOKUP
-#define NOKOGIRI_HTML_ENTITY_LOOKUP
-
-#include <native.h>
-
-void init_html_entity_lookup();
-
-#endif
--- vendor/gems/nokogiri-1.2.3/lib/nokogiri/version.rb
+++
@@ -1,4 +0,0 @@
-module Nokogiri
-  # The version of Nokogiri you are using
-  VERSION = '1.2.3'
-end
