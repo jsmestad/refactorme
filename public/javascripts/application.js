58fd3e11c892a23b9a547a64e414f4eaec17a87c
jsmestad <justin.smestad@gmail.com>
2009-03-30 16:05:54 -0600

    updated javascript due to markup changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -33,7 +33,7 @@ jQuery(document).ready(function($) {
   });
   
   $('a.fork').live('click', function() {
-    $(this).closest('ul').next("div.hidden").slideToggle("slow"); 
+    $(this).closest('div.details').next("div.hidden").slideToggle("slow"); 
     return false;
   });
   
