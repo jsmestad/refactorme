5941706e500e14a47769c9e04d4aba835db10af6
jsmestad <justin.smestad@gmail.com>
2009-03-30 21:48:01 -0600

    redirecting to account? no send to root as user does not exist when reseting password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -28,7 +28,7 @@ class PasswordResetsController < ApplicationController
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.save
       flash[:notice] = "Password successfully updated"
-      redirect_to account_url
+      redirect_to login_url
     else
       render :action => :edit
     end
