5767ace730ae54a704f8aa9abec0deee4c21463d
jsmestad <justin.smestad@gmail.com>
2009-03-30 22:24:08 -0600

    undoing more stupid rails defaults

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -16,7 +16,7 @@ class ActivationsController < ApplicationController
     if @user.update_attributes( params[:user].merge({:active => true}) )
       @user.deliver_activation_confirmation!
       flash[:success] = "Your account has been activated."
-      redirect_to @user
+      redirect_to user_path(@user)
     else
       render :action => 'new'
     end
