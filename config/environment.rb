208623884a58c3d7123cd9b3df1a44685eedfde5
jsmestad <justin.smestad@gmail.com>
2009-03-31 22:57:16 -0600

    trying something new

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,6 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 require 'validations'
-require 'rack/cache'
 
 Rails::Initializer.run do |config|
   # config.middleware.use(Rack::Cache,
