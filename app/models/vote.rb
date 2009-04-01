24ca6fa4e8d0738be58f13f5b3ee2f48220cc4ac
Gabe Varela <gabevarela@gabe.local>
2009-04-01 10:16:06 -0600

    added vote_cache_score to refactors, moved sql error to warning.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,6 +4,13 @@ class Vote < ActiveRecord::Base
   
   validates_presence_of :score
   
+  after_save :update_score_cache
+  
   named_scope :positive, :conditions => ["score > 0"]
   named_scope :negative, :conditions => ["score < 0"]
+  
+  private
+    def update_score_cache
+      self.refactor.update_attribute( :vote_score_cache, self.refactor.vote_score_cache + self.score )
+    end
 end
--- spec/models/refactor_spec.rb
+++ spec/models/refactor_spec.rb
@@ -4,12 +4,30 @@ describe Refactor do
   
   before(:each) do
     @valid_attributes = Factory.attributes_for(:refactor)
-    Factory.build(:refactor)
+    @refactor = Factory.build(:refactor)
   end
   
   it { should belong_to(:user) }
   it { should belong_to(:snippet) }
   it { should validate_presence_of(:code) }
   
+  it "should have 0 for score cache" do
+    @refactor.vote_score_cache.should == 0
+  end
+  
+  describe "score cache" do
+    before(:each) do
+      @refactor.save
+      @refactor.votes.create(:score => 1)
+    end
+    
+    it "should have 1 for score cache" do
+      @refactor.reload
+      @refactor.vote_score_cache.should == 1
+    end
+    
+  end
+  
   
 end
+
