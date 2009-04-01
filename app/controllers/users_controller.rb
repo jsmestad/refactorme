1f43ecb270507b6dd67d193512c583cedba9c314
Gabe Varela <gabevarela@gabe.local>
2009-04-01 10:16:06 -0600

    added vote_cache_score to refactors, moved sql error to warning.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_login!(params[:id])
     @user_snippets = @user.snippets.find(:all, :conditions => ["displayed_on IS NOT NULL"], :limit => 5, :order => 'created_at DESC')
-    @top_refactors = @user.refactors.find(:all, :limit => 5, :joins => :votes, :order => "SUM(votes.score) DESC")
+    @top_refactors = @user.refactors.find(:all, :limit => 5, :order => "vote_score_cache DESC")
   end
 
   # def edit
