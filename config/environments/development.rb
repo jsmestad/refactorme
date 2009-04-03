3f85ee26de19d053c17497b0f1c12e02d8c98f7e
jsmestad <justin.smestad@gmail.com>
2009-04-02 19:49:18 -0600

    setup caching of gists

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,11 @@ config.whiny_nils = true
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_view.debug_rjs                         = true
-config.action_controller.perform_caching             = false
+config.action_controller.perform_caching             = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 config.action_mailer.perform_deliveries = false
+
+# Cache on local machine when enabled
+config.cache_store = :mem_cache_store
