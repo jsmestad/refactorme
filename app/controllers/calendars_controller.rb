0f8172c99a55061ba7ef6631ecdadcc00fc2ada9
jsmestad <justin.smestad@gmail.com>
2009-04-02 20:03:16 -0600

    added calendar page caching

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,7 @@
 class CalendarsController < ApplicationController
+  
+  caches_page :index
+  
   def index
     @date = Date.today
     @snippets = Snippet.all(:conditions => ["MONTH(displayed_on) = ?", Date.today.month], :order => 'displayed_on ASC')
