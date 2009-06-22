d01ef6f6fea44469582863ed8a675d6fd9993786
jsmestad <justin.smestad@gmail.com>
2009-06-22 14:58:50 -0600

    auth works, session not maintained

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,21 @@ class ApplicationController < ActionController::Base
   helper :all
   protect_from_forgery
 
-  helper_method :current_user_session, :current_user
   filter_parameter_logging :password, :password_confirmation
 
   rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 
   private
   
-    def current_user_session
-      return @current_user_session if defined?(@current_user_session)
-      @current_user_session = UserSession.find
-    end
+    #def current_user_session
+      #return @current_user_session if defined?(@current_user_session)
+      #@current_user_session = UserSession.find
+    #end
 
-    def current_user
-      return @current_user if defined?(@current_user)
-      @current_user = current_user_session && current_user_session.record
-    end
+    #def current_user
+      #return @current_user if defined?(@current_user)
+      #@current_user = current_user
+    #end
     
     def require_admin
       unless current_user && current_user.is_admin?
@@ -30,7 +29,8 @@ class ApplicationController < ActionController::Base
     end
 
     def require_user
-      unless current_user
+      debugger
+      if current_user.nil?
         #store_location
         flash[:notice] = "You must be logged in to access this page"
         redirect_to login_url
@@ -39,16 +39,12 @@ class ApplicationController < ActionController::Base
     end
 
     def require_no_user
-      if current_user
+      unless current_user.nil?
         #store_location
         flash[:notice] = "You must be logged out to access this page"
         redirect_to root_url
         return false
       end
-    end
-
-    def store_location
-      session[:return_to] = request.request_uri
     end
 
     def redirect_back_or_default(default)
